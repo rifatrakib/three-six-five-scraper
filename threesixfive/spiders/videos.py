20f6ea2ae15f85b511585b871b4b614f09a70ac2
rifatrakib <Rakib.1508@outlook.com>
2022-11-19 21:05:50 +0600

    ignore video files for new video downloader

---
+++ threesixfive/spiders/videos.py
@@ -0,0 +1,47 @@
+import json
+from pathlib import Path
+
+import scrapy
+
+from threesixfive.spiders.utils import read_secrets
+
+
+class VideosSpider(scrapy.Spider):
+    name = "videos"
+
+    def start_requests(self):
+        with open("data/playbacks/video.json") as reader:
+            data = json.loads(reader.read())
+
+        secrets = read_secrets(self.name)
+        headers, cookies = secrets["headers"], secrets["cookies"]
+
+        for doc in data:
+            url = doc["src"]
+            extra_data = {
+                "course_name": doc["course_name"],
+                "chapter_name": doc["chapter_name"],
+                "lesson_name": doc["lesson_name"],
+            }
+
+            yield scrapy.Request(
+                url=url,
+                headers=headers,
+                cookies=cookies,
+                callback=self.parse,
+                cb_kwargs=extra_data,
+            )
+
+    def parse(self, response, **kwargs):
+        course_name = kwargs["course_name"]
+        chapter_name = kwargs["chapter_name"]
+        lesson_name = kwargs["lesson_name"]
+        data = response.body
+
+        location = f"data/videos/{course_name}/{chapter_name}"
+        Path(location).mkdir(parents=True, exist_ok=True)
+
+        with open(f"{location}/{lesson_name}.mp4", "wb") as writer:
+            writer.write(data)
+
+        print(f"Saved new video {location}/{lesson_name}.mp4")
